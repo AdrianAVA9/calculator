02f3dac2a9e65019dbfaee525b4fb2bdee331d61
Adrian Vega <adrian.vegaa@outlook.com>
2024-06-18 21:01:12 -0600

    Implemented services for performing calculator operations.

---
+++ Calculator.UI/Models/Api/ApiResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace Calculator.UI.Models.Api
+{
+    public class ApiResponse
+    {
+        public bool Ok { get; set; }
+        public string Message { get; set; }
+        public object Result { get; set; }
+    }
+}
---
+++ Calculator.UI/Validators/OperationValidator.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Calculator.UI.Validators
+{
+    public class OperationValidator
+    {
+        [Required]
+        [Range(0, float.MaxValue, ErrorMessage = "Please enter valid integer Number")]
+        public int FirstNumber { get; set; }
+
+        [Required]
+        [Range(0, float.MaxValue, ErrorMessage = "Please enter valid integer Number")]
+        public int SecondNumber { get; set; }
+
+        [Required]
+        [RegularExpression("[-+x/]", ErrorMessage = "Invalid operation")]
+        public string? Operation { get; set; }
+    }
+}
