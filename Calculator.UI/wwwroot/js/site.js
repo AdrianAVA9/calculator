4462813228dca0dcaec4d3dfc18a969b82f4041c
Adrian Vega <adrian.vegaa@outlook.com>
2024-06-18 21:02:04 -0600

    Added error messages

--- Calculator.UI/wwwroot/js/site.js
+++ Calculator.UI/wwwroot/js/site.js
@@ -2,19 +2,43 @@
     let _calculator;
 
     let makeOperation = function () {
-        let firstNumber = _calculator.find('.js-first-number').val();
-        let secondNumber = _calculator.find('.js-second-number').val();
-        let operation = _calculator.find('.js-operation').val();
+        let data = JSON.stringify({
+            firstNumber: _calculator.find('.js-first-number').val(),
+            secondNumber: _calculator.find('.js-second-number').val(),
+            operation: _calculator.find('.js-operation').val()
+        });
 
-        $.post('/api/calculator/make-operation', {
-            firstNumber, secondNumber, operation
+        //Reset error messages
+        $('.error-message').html('');
+
+        $.post({
+            url: '/api/calculators/make-operation',
+            headers: {
+                'content-type': 'application/json'
+            },
+            data
+
         }).then((response) => {
             console.log(response);
-
             const result = response.result;
-            _calculator.find('.js-result').html(result);
+            _calculator.find('.js-result').html(`The result is: ${result}`);
+
+            if (!response.ok)
+                alert(response.message)
+
         }).fail(error => {
-            console.log(error);
+            let errors = error.responseJSON?.errors ? error.responseJSON.errors : {};
+
+            if (error.responseJSON.status === 400) {
+                $('.error-message').each((index, element) => {
+                    let name = $(element).attr('name');
+                    $(element).html('');
+
+                    if (errors[name]) {
+                        $(element).html(errors[name].join(' | '));
+                    }
+                });
+            }
         });
     };
 
